4cdfcc000d9192b40702d5049e744c78217b5f7d
pyloaduser@gmail.com <pyloaduser@gmail.com@9e91852e-afd7-bd9e-f989-0c5a545c4049>
2012-05-11 22:49:36 +0000

    Kleinere Anpassungen

--- runscript.sh
+++ runscript.sh
@@ -2,19 +2,20 @@
 
 #---------------------------------------------------------------------------
 #------- Script "bereinigt" Datei / Ordner -  Namen und entfernt
-#------- ungewollte u. unoetige Dateien / Dateinamen-Parts wie
+#------- ungewollte u. unoetige Dateien und Dateinamen-Parts wie
 #------- Scene-Tags oder Sprachangaben und bringt die Dateien in
 #------- eine saubere Dateikultur
 #-------
 #------- Initial-Script für pyLoad "package_finished" / "unrar_finished"
-#------- findet ihr unter http://pastebin.com/veq9miyX
+#------- http://code.google.com/p/pyload-runscript/source/browse/start.sh
 #------- 
 #------- aktuelle "ipkg tr" "ipkg sed" "ipkg find" Pakete vorausgesetzt!
 #------- "ipkg update" "ipkg install tr sed find"
 #---------------------------------------------------------------------------
 
 #---------------------------------------------------------------------------
-#------- persoenliche Pfadangaben in http://pastebin.com/veq9miyX bearbeiten
+#------- persoenliche Pfadangaben bearbeiten
+#------- http://code.google.com/p/pyload-runscript/source/browse/start.sh
 #---------------------------------------------------------------------------
 
 PATH=/opt/bin:/opt/sbin
@@ -34,41 +35,41 @@ cd "$UF_FOLDER"
 
 renRecursive()
 {
-/opt/bin/find `$UF_FOLDER/`* ! -name "*.sh" -type d -print | while read FILE
+find `$UF_FOLDER/`* ! -name "*.sh" -type d -print | while read FILE
 do
-if [ -d $FILE ]; then
-        cd $FILE
-        renRecursive
-        renFolder
-        renFiles
-        cd ..
-fi
+  if [ -d $FILE ]; then
+    cd $FILE
+    renRecursive
+    renFolder
+    renFiles
+    cd ..
+  fi
 done
 }
 
 renFolder()
 {
-/opt/bin/find `$UF_FOLDER/`* ! -name "*.sh" -type d -print | while read FILE
+find `$UF_FOLDER/`* ! -name "*.sh" -type d -print | while read FILE
 do
-if [ -d "$FILE" ]; then
-        NEW=`echo "$FILE" | sed -r 's/ +/./g' | sed -r 's/-/./g;s/_/./g;s/^\.+//;s/\.+$//' | tr -s '.'`
-        if [ $NEW != $FILE -a ! -f $NEW -a ! -d $NEW ]; then
-        mv "$FILE" "$NEW"
-	fi
-fi
+  if [ -d "$FILE" ]; then
+  NEW=`echo "$FILE" | sed -r 's/ +/./g' | sed -r 's/-/./g;s/_/./g;s/^\.+//;s/\.+$//' | tr -s '.'`
+    if [ $NEW != $FILE -a ! -f $NEW -a ! -d $NEW ]; then
+      mv "$FILE" "$NEW"
+    fi
+  fi
 done
 }
 
 renFiles()
 {
-/opt/bin/find `$UF_FOLDER/`* ! -name "*.sh" -type f -print | while read FILE
+find `$UF_FOLDER/`* ! -name "*.sh" -type f -print | while read FILE
 do
-if [ -f "$FILE" ]; then
-        NEW=`echo "$FILE" | sed -r 's/ +/./g' | sed -r 's/-/./g;s/_/./g;s/^\.+//;s/\.+$//' | tr -s '.'`
-        if [ $NEW != $FILE -a ! -f $NEW -a ! -d $NEW ]; then
-        mv "$FILE" "$NEW"
-	fi
-fi
+  if [ -f "$FILE" ]; then
+  NEW=`echo "$FILE" | sed -r 's/ +/./g' | sed -r 's/-/./g;s/_/./g;s/^\.+//;s/\.+$//' | tr -s '.'`
+    if [ $NEW != $FILE -a ! -f $NEW -a ! -d $NEW ]; then
+      mv "$FILE" "$NEW"
+    fi
+  fi
 done
 }
 
@@ -78,46 +79,46 @@ done
 
 lowRecursive()
 {
-/opt/bin/find `$UF_FOLDER/`* -name "*" -type d -print | while read FILE
+find `$UF_FOLDER/`* -name "*" -type d -print | while read FILE
 do
-if [ -d $FILE ]; then
-        cd $FILE
-        lowRecursive
-        lowFiles
-        cd ..
-    fi
+  if [ -d $FILE ]; then
+    cd $FILE
+    lowRecursive
+    lowFiles
+    cd ..
+  fi
 done
 }
 
 lowFiles()
 {
-/opt/bin/find `$UF_FOLDER/`* ! -name "*.sh" | while read FILE
+find `$UF_FOLDER/`* ! -name "*.sh" | while read FILE
 do
-if [ -f $FILE -o -d $FILE ]; then
-        NEW=`echo $FILE | tr 'A-Z' 'a-z'`
-        if [ $NEW != $FILE -a ! -f $NEW -a ! -d $NEW ]; then
-        mv "$FILE" "$NEW"
-	fi
-fi
+  if [ -f $FILE -o -d $FILE ]; then
+  NEW=`echo $FILE | tr 'A-Z' 'a-z'`
+    if [ $NEW != $FILE -a ! -f $NEW -a ! -d $NEW ]; then
+      mv "$FILE" "$NEW"
+    fi
+  fi
 done
 }
 
 #---------------------------------------------------------------------------
 #------- entferne Szene-Tags und / oder unnoetige Dateinamenteile
-#------- "$DELNAME" Pfadangabe unter http://pastebin.com/veq9miyX
-#------- anpassen und die entsprechende Datei erstellen / editieren
+#------- "$DELNAME" ggf. anlegen und mit zu filternden Inhalten fuellen
+#------- http://code.google.com/p/pyload-runscript/source/browse/start.sh
 #---------------------------------------------------------------------------
 
 rmTagsRecursive()
 {
-/opt/bin/find `$UF_FOLDER/`* ! -name "*.sh" -type d -print | while read FILE
+find `$UF_FOLDER/`* ! -name "*.sh" -type d -print | while read FILE
 do
-if [ -d $FILE ]; then
-        cd "$FILE"
-        rmTagsRecursive
-        rmTags
-        cd ..
-fi
+  if [ -d $FILE ]; then
+    cd "$FILE"
+    rmTagsRecursive
+    rmTags
+    cd ..
+  fi
 done
 }
 
@@ -125,21 +126,21 @@ rmTags()
 {
 for i in `cat $DELNAME`
 do
-/opt/bin/find `$UF_FOLDER/`* ! -name "*.sh" -print | while read FILE
-do
-if [ -d $FILE ]; then
-	NEW=`echo $FILE | sed -r 's/'"$i"'\.//g' | sed -r 's/\.'"$i"'//g'`
+  find `$UF_FOLDER/`* ! -name "*.sh" -print | while read FILE
+    do
+      if [ -d $FILE ]; then
+      NEW=`echo $FILE | sed -r 's/'"$i"'\.//g' | sed -r 's/\.'"$i"'//g'`
         if [ $NEW != $FILE -a ! -f $NEW -a ! -d $NEW ]; then
-	mv "$FILE" "$NEW"
-	fi
-fi
-if [ -f $FILE ]; then
-	NEW=`echo $FILE | sed -r 's/'"$i"'\.//g' | sed -r 's/'"$i"'//g'`
+          mv "$FILE" "$NEW"
+        fi
+      fi
+      if [ -f $FILE ]; then
+      NEW=`echo $FILE | sed -r 's/'"$i"'\.//g' | sed -r 's/'"$i"'//g'`
         if [ $NEW != $FILE -a ! -f $NEW -a ! -d $NEW ]; then
-	mv "$FILE" "$NEW"
-	fi
-fi
-done
+          mv "$FILE" "$NEW"
+        fi
+      fi
+    done
 done
 }
 
@@ -149,74 +150,74 @@ done
 
 upRecursive()
 {
-/opt/bin/find `$UF_FOLDER/`* ! -name "*.sh" -type d -print | while read FILE
+find `$UF_FOLDER/`* ! -name "*.sh" -type d -print | while read FILE
 do
-if [ -d $FILE ]; then
-        cd "$FILE"
-        upRecursive
-        upFolder
-        upFiles
-        cd ..
-fi
+  if [ -d $FILE ]; then
+    cd "$FILE"
+    upRecursive
+    upFolder
+    upFiles
+    cd ..
+  fi
 done
 }
 
 upFolder()
 {
-/opt/bin/find `$UF_FOLDER/`* ! -name "*.sh" -type d -print | while read FILE
+find `$UF_FOLDER/`* ! -name "*.sh" -type d -print | while read FILE
 do
-   if [ -d $FILE ]; then
-        # nachfolgende Codezeile darf nicht "gelinebreakt" werden!
-        NEW=`echo $FILE | sed -r 's/(\<.)/\u\1/g;s/([se][0123456789])/\u\1/g;s/Dvd/DVD/;s/Dts/DTS/;s/Csi/CSI/;s/Hd/HD/'`
-        if [ $NEW != $FILE -a ! -f $NEW -a ! -d $NEW ]; then
-        mv "$FILE" "$NEW"
-	fi
-   fi
+  if [ -d $FILE ]; then
+  # nachfolgende Codezeile darf nicht "gelinebreakt" werden!
+  NEW=`echo $FILE | sed -r 's/(\<.)/\u\1/g;s/([se][0123456789])/\u\1/g;s/Dvd/DVD/;s/Dts/DTS/;s/Csi/CSI/;s/Hd/HD/'`
+    if [ $NEW != $FILE -a ! -f $NEW -a ! -d $NEW ]; then
+      mv "$FILE" "$NEW"
+    fi
+  fi
 done
 }
 
 upFiles()
 {
-/opt/bin/find `$UF_FOLDER/`* ! -name "*.sh" -type f -print | while read FILE
+find `$UF_FOLDER/`* ! -name "*.sh" -type f -print | while read FILE
 do
-   if [ -f $FILE ]; then
-        # nachfolgende Codezeile darf nicht "gelinebreakt" werden!
-	NEW=`echo $FILE | sed -r 's/(\<.)/\u\1/g;s/([se][0123456789])/\U\1/g;s/Dvd/DVD/;s/Dts/DTS/;s/Hd/HD/;s/Csi/CSI/' | sed -r 's/(\....)$/\L\1/'`
-        if [ $NEW != $FILE -a ! -f $NEW -a ! -d $NEW ]; then
-	mv "$FILE" "$NEW"
-	fi
-   fi
+  if [ -f $FILE ]; then
+  # nachfolgende Codezeile darf nicht "gelinebreakt" werden!
+  NEW=`echo $FILE | sed -r 's/(\<.)/\u\1/g;s/([se][0123456789])/\U\1/g;s/Dvd/DVD/;s/Dts/DTS/;s/Hd/HD/;s/Csi/CSI/' | sed -r 's/(\....)$/\L\1/'`
+    if [ $NEW != $FILE -a ! -f $NEW -a ! -d $NEW ]; then
+      mv "$FILE" "$NEW"
+    fi
+  fi
 done
 }
 
 renRecursive
-sleep 1
+ sleep 1
 renFolder
-sleep 1
+ sleep 1
 renFiles
 
-sleep 1
+ sleep 1
 lowRecursive
-sleep 1
+ sleep 1
 lowFiles
 
-sleep 1
+ sleep 1
 rmTagsRecursive
-sleep 1
+ sleep 1
 rmTags
 
-sleep 1
+ sleep 1
 renRecursive
-sleep 1
+ sleep 1
 renFolder
-sleep 1
+ sleep 1
 renFiles
 
-sleep 1
+ sleep 1
 upRecursive
-sleep 1
+ sleep 1
 upFolder
-sleep 1
+ sleep 1
 upFiles
 
 #---------------------------------------------------------------------------
@@ -225,18 +226,18 @@ upFiles
 #---------------------------------------------------------------------------
 
 sleep 5
-/opt/bin/find `$UF_FOLDER/`* -iname "Runscript.sh" -print | while read FILE
+find `$UF_FOLDER/`* -iname "Runscript.sh" -print | while read FILE
 do
-rm "$FILE"
+  rm "$FILE"
 done
 
 copy2destination()
 {
-echo "`date` /opt/bin/find `$UF_FOLDER/`* ! -iname "*.sh"" >> $LOGFILE 
-/opt/bin/find `$UF_FOLDER/`* ! -iname "*.sh" -print | while read FILE
+echo "`date` find `$UF_FOLDER/`* ! -iname "*.sh"" >> $LOGFILE 
+find `$UF_FOLDER/`* ! -iname "*.sh" -print | while read FILE
 do
-        echo "mv "$FILE" "$DESTINATION/"" >> $LOGFILE
-        mv "$FILE" "$DESTINATION/"
+  echo "mv "$FILE" "$DESTINATION/"" >> $LOGFILE
+  mv "$FILE" "$DESTINATION/"
 done
 }
 
@@ -247,12 +248,12 @@ do
 #---------------------------------------------------------------------------
 #------- entferne "-type f" um die Datei inkl. Ordner, falls vorhanden, zu verschieben
 #---------------------------------------------------------------------------
-echo "`date` /opt/bin/find `$UF_FOLDER/`* -iname "*"$SERIEN"*" -type f" >> $LOGFILE
-/opt/bin/find `$UF_FOLDER/`* -iname "*"$SERIEN"*" -type f -print | while read SERIE
-        do
-	echo "mv "$SERIE" "$DESTISERIEN/"$SERIEN"/"" >> $LOGFILE
-        mv "$SERIE" "$DESTISERIEN/"$SERIEN"/"
-        done
+echo "`date` find `$UF_FOLDER/`* -iname "*"$SERIEN"*" -type f" >> $LOGFILE
+find `$UF_FOLDER/`* -iname "*"$SERIEN"*" -type f -print | while read SERIE
+  do
+    echo "mv "$SERIE" "$DESTISERIEN/"$SERIEN"/"" >> $LOGFILE
+    mv "$SERIE" "$DESTISERIEN/"$SERIEN"/"
+  done
 done
 }
 copyseries
@@ -262,13 +263,12 @@ copyseries
 #---------------------------------------------------------------------------
 
 if [ $? -eq 0 ] ; then
- sleep 25; rm -rf "$UF_FOLDER"
+  sleep 25; rm -rf "$UF_FOLDER"
 else
- copy2destination 
-fi
-
-if [ $? -eq 0 ] ; then
-sleep 25; rm -rf "$UF_FOLDER"
+  copy2destination 
+    if [ $? -eq 0 ] ; then
+      sleep 25; rm -rf "$UF_FOLDER"
+    fi
 fi
 
 #---------------------------------------------------------------------------
